dd5b796f097769a17cd52fb68caf9c32319bbb0e
Mpho MATHIVHA <mmathivh@e4r15p9.wethinkcode.co.za>
2018-10-06 16:05:09 +0200

    fix admin

--- admin.php
+++ admin.php
@@ -1,14 +1,16 @@
 <?php include "includes/header.php"; ?>
     <div class= "c_nav">
-        <h2 id="adidas">Your Basket</h2>
+        <h2 id="adidas">ADMIN PRODUCT</h2>
     </div>
     <div class= "c_nav">
     <li class="basket">
            <ul>
+               <form action="update_product.php" method="get">
                 <div class="item-sec"><img src="img/shoe1.jpeg" height="50px"/></div>
-               <div class="item-sec">Shoes 204</div>
-               <div class="item-sec">R 350.00</div>
-               <div class="item-sec"><a href="remove.php?productno=123"> Remove</a><a href="update.php?productno=123" class="green">Update</a></div>
+               <input class="item-sec" value="Shoes"/>
+               <input type="text" class="item-sec" value="R 350.00"/>
+               <div class="item-sec"><input type="submit" name="remove" value="remove"class="red"/><input type="submit" name="update"class="green" value="add"/></div>
+                </form>
            </ul>
            <ul>
                <div class="item-sec"><b>ADD PRODUCT </b></div>
