9c775440a7fd562e37ac54e4e8ecb2a603aa4bf0
Mpho MATHIVHA <mmathivh@e4r15p9.wethinkcode.co.za>
2018-10-06 12:36:49 +0200

    rush00 submit

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script src="main.js"></script>
+</head>
+    <body>
+    <header>
+    <nav>
+        <div class= "c_nav">
+        <a class="t_links" href=""><img src="img/logo.png" height="60px" /></a>
+            <a class="t_links" href="">HOME</a>
+            <a class="t_links" href="cat_page.php">PRODUCTS</a>
+            <a class="t_links" href="">SIGN IN</a>
+            <a class="t_links" href="">BASKET</a>
+            <span class="t_links">
+                <input type="text" placeholder="Search" name="search"/>
+            </span>
+        </div>
+    </nav>
+    </header>
+    <div class="corra">
+    <div class="corra_img">
+        <div class="cor_text">
+            <h2>Prime sneaker</h2>
+            <a class="cor_btn"> Buy Now</a>
+        </div>
+        <img src="img/home_sneaker.jpg" height="400px"/>
+    </div>
+    </div>
+    <div class= "c_nav">
+        <h2>Our Collection</h2>
+    </div>
+    <div class= "c_navc">
+        <div class="item">
+        <a class="cor_text2" href="cat_page.php">Adidas</a>
+        <img src="img/shoe1.jpeg" width="100%"/>
+        </div>
+
+        <div class="item">
+        <a class="cor_text2" href="cat_page.php">Nike</a>
+        <img src="img/shoe1.jpeg" width="100%"/>
+        </div>
+
+        <div class="item">
+        <a class="cor_text2" href="cat_page.php">Puma</a>
+        <img src="img/shoe1.jpeg" width="100%"/>
+        </div>
+
+        <div class="item">
+        <a class="cor_text2" href="cat_page.php">New balance</a>
+        <img src="img/shoe1.jpeg" width="100%"/>
+        </div>
+
+        <div class="item">
+        <a class="cor_text2" href="cat_page.php">Converse</a>
+        <img src="img/shoe1.jpeg" width="100%"/>
+        </div>
+
+        <div class="item">
+        <a class="cor_text2" href="cat_page.php">Rebook</a>
+        <img src="img/shoe1.jpeg" width="100%"/>
+        </div>
+    </div>
+    <div class= "c_nav">
+        <hr>
+        <footer>
+       <li>
+           <ul><b><a href="index.php">Home</a></b></ul>
+       </li>
+       <li>
+           <ul><b>Catagories</b></ul>
+           <ul><a href="cat_page.php#reebok">Reebok</a></ul>
+           <ul><a href="cat_page.php#newbalance">New Balance</a></ul>
+           <ul><a href="cat_page.php#adidas">Adidas</a></ul>
+           <ul><a href="cat_page.php#nike">Nike</a></ul>
+       </li>
+       <li>
+           <ul><b>Login</b></ul>
+           <ul><a href="signup.php">Sign Up</a></ul>
+           <ul><a href="signin.php">Sign In</a></ul>
+       </li>
+       <li>
+           <ul><b><a href="basket.php">Basket</a></b></ul>
+       </li>
+       
+        </footer> 
+        <div class="clear"></div>
+        <hr>
+        <span class="copy">mmathivh and tstepen © 2018 </span>
+    </div>
+    </body>
+</html>
