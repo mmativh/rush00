2e2e5afa8a0c4fbfd614d9b427ef6cd9924a9f5f
Mpho MATHIVHA <mmathivh@e5r10p4.wethinkcode.co.za>
2018-10-07 18:15:21 +0200

    add adminss

--- includes/user_update.php
+++ includes/user_update.php
@@ -2,9 +2,7 @@
    include('connect.php');
    $lname = $_POST['lname'];
    $fname = $_POST['fname'];
-   $email = $_POST['email'];
    $user_id = $_POST['id'];
-   $passwd = $_POST['passwd'];
 
    if ($_POST['sumbit'])
    {
@@ -12,6 +10,7 @@
 
         if (mysqli_query($conn, $sql)) {
             echo "Record updated successfully";
+            header("Location: ../index.php");
         } else {
             echo "Error updating record: " . mysqli_error($conn);
         }
