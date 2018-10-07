45c4bc3c233a26d2b7e218ff8f51e723bc803b45
Mpho MATHIVHA <mmathivh@e5r10p4.wethinkcode.co.za>
2018-10-07 15:54:04 +0200

    add admin

--- includes/admin_list.php
+++ includes/admin_list.php
@@ -1,14 +1,62 @@
 <?php
+include('connect.php');
 
+$sql = "SELECT id, category, productName, price, stock FROM productinfo";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+    $p_units = $row["stock"];
+    $p_price = $row["price"];
+    $p_id = $row["id"];
+    $p_name = $row["productName"];
+    $cat = $row["category"];
+
+        echo '<form action="includes/product_update.php" method="POST" enctype="multipart/form-data">
+    <div class="item-sec">
+        <img src="img/shoe1.jpeg" height="50px"/>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+    </div>
+    <div class="item-sec">
+        <label class="a_label">Name of product:</label>
+        <input type="text" name="product_name" value="'.$p_name.'"/>
+        <label class="a_label">Category:</label>
+        <select name="cat" value="'.$cat.'">
+        <option value="puma">Puma</option>
+        <option value="nike">Nike</option>
+        <option value="addidas">Adidas</option>
+        <option value="newbalance">New Balance</option>
+        <option value="reebok">Reebok</option>
+        </select>
+    </div>
+    <div class="item-sec">
+        <label class="a_label">Price of product:</label>
+        <input type="text" name="price" value="'.$p_price.'"/>
+        <label class="a_label">Units available:</label>
+        <input class="unit_num" type="text" name="num_items" value="'.$p_units.'"/>
+    </div>
+    <div class="item-sec">
+        <input type="submit" name="update" class="green" value="Update"/>
+        <a href="includes/product_delete.php?product_id='.$p_id.'" class="red">Remove</a>
+    </div>
+    </form>';
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
 for ($num = 0; $num < 6; $num++)
 {
     $p_units = 5;
-    $p_price = 500.00;
+    $p_price = 500;
     $p_id = 1230;
     $p_name = "Puma";
     $p_image = "shoe1.jpeg";
 
-    echo '<form action="update_product.php" method="POST" enctype="multipart/form-data">
+    echo '<form action="includes/product_update.php" method="POST" enctype="multipart/form-data">
     <div class="item-sec">
         <img src="img/'.$p_image.'" height="50px"/>
         <input type="file" name="fileToUpload" id="fileToUpload">
@@ -33,7 +81,7 @@ for ($num = 0; $num < 6; $num++)
     </div>
     <div class="item-sec">
         <input type="submit" name="update" class="green" value="Update"/>
-        <a href="delete_item.php?productid='.$p_id.'" class="red">Remove</a>
+        <a href="includes/product_delete.php?product_id='.$p_id.'" class="red">Remove</a>
     </div>
     </form>';
 }
---
+++ includes/connect.php
@@ -0,0 +1,10 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "w1Shoni98";
+	$dbname = "mydb";
+
+	// create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
